0b5370ab475063c89130e149f8e2614206847289
CoreTechSolution <rktechnology2010@gmail.com>
2018-08-03 16:15:23 +0530

    New theme for the New Design of the Website

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,4 @@
+<?php exit; ?>
+[2018-06-05 11:22:10] ERROR: Form 51270 > MailChimp API error: 400 Bad Request. Invalid Resource. test@ma********.com looks fake or invalid, please enter a real email address.
+Request: PUT https://us16.api.mailchimp.com/3.0/lists/5998e8fb3c/members/723d42f66c080b02ce346ba59bd5b9ef - {"email_address":"test@ma********.com","interests":{},"merge_fields":{},"status":"pending","email_type":"html","ip_signup":"110.227.106.202"}
+Response: 400 Bad Request - {"type":"http://developer.mailchimp.com/documentation/mailchimp/guides/error-glossary/","title":"Invalid Resource","status":400,"detail":"test@ma********.com looks fake or invalid, please enter a real email address.","instance":"723b50d2-ce11-4c08-8695-24e66d358fa1"}
